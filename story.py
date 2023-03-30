c65553706da1e20acc9da68af026ff514cbe51ed
Benny Bach <92464226+bennyest@users.noreply.github.com>
2023-03-30 15:32:50 -0300

    Initial commit

---
+++ story.py
@@ -0,0 +1,22 @@
+loop = 1
+while (loop < 10):
+
+    noun = input("Choose a noun: ")
+    p_noun = input("Choose a plural noun: ")
+    verb = input("Choose a verb: ")
+    noun2 = input("Choose a noun: ")
+    place = input("Name a place: ")
+    adjective = input("Choose an adjective (Describing word): ")
+    noun3 = input("Choose a noun: ")
+    relative = input("Choose a relative: ")
+
+    print ("Be nice to your",noun,"my fellow", p_noun)
+    print ("For a" ,noun," may be somebody's", noun2,",")
+    print ("Help your",p_noun,"in",place)
+    print ("Because the weather is always",adjective,".")
+    print ("You may think that is this the",noun3,",")
+    print ("Well it is.")
+    print("So for that, always" ,verb , "your" , relative)
+    print("Tchau for now")
+
+    loop = loop + 1
